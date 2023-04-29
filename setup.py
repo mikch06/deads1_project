b49f1c7c5997a0f8ad596be2924e8217460562b6
mikch06 <tools@kissel.ch>
2023-04-29 18:20:45 +0200

    add requirements.txt and setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='deades1-project',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='mki',
+    author_email='',
+    description=''
+)
