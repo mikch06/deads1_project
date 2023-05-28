bcd7b399c99131d1000ca2cfe07fef0b51e216dd
mikch06 <tools@kissel.ch>
2023-05-28 17:53:04 +0200

    init pandas tail

--- scripts/check_pandas.py
+++ scripts/check_pandas.py
@@ -1,12 +1,22 @@
 import pandas as pd
-import ydata_profiling
+df = pd.read_json("../data/landing/data.ch.bfe.ladestellen-elektromobilitaet.json")
+print(df.shape)
+print(df.tail)
+df = pd.read_json("../data/landing/status.ch.bfe.ladestellen-elektromobilitaet.json")
+print(df.shape)
+print(df.tail)
+df = pd.read_csv("../data/landing/ich_tanke_strom_monthly.csv")
+print(df.shape)
+print(df.tail)
 
-df = pd.read_csv("../raw/csv/kantonsratswahl-2022-gde-risch.csv")
+
+
+
 
 #df2 = pd.read_csv("../raw/csv/kantonsratswahl-2022-gde-risch.csv")
 # profile = df.profile_report(html={'style':{'full_width':True}})
 # profile.to_file(output_file="sgv-profile.html")
-print(df.shape)
+print(df.tail)
 print(df.head(n=5))
 
 visual = df.to_html("foo.html")
