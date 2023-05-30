43a31a6b194aee7290f87c6b8792a0c93c7ebfdb
mikch06 <tools@kissel.ch>
2023-05-30 11:10:52 +0200

    fix

--- scripts/check_pandas.py
+++ scripts/check_pandas.py
@@ -10,14 +10,3 @@ print(df.shape)
 print(df.tail)
 
 
-
-
-
-#df2 = pd.read_csv("../raw/csv/kantonsratswahl-2022-gde-risch.csv")
-# profile = df.profile_report(html={'style':{'full_width':True}})
-# profile.to_file(output_file="sgv-profile.html")
-print(df.tail)
-print(df.head(n=5))
-
-visual = df.to_html("foo.html")
-print(visual)
