87da3e7d101a525998822115cfa80ac968209c22
mik <mail@kissel.ch>
2023-06-01 22:43:45 +0200

    init b_ichtankestrom.py

---
+++ scripts/b_ichtankestrom.py
@@ -0,0 +1,17 @@
+import pandas as pd
+# datainfo
+# stations_XY_count : Anzahl Ladestationen pro Kanton.
+# locations_XY_count : Anzahl Standorte pro Kanton.
+
+df = pd.read_csv("../data/landing/ich_tanke_strom_monthly.csv")
+
+df1 = df.filter(regex="(year|month|locations_)")
+pd.set_option("display.max_rows", None)
+pd.set_option('display.max_columns', None)
+
+df2 = df.filter(regex="(year|month|stations_)")
+pd.set_option("display.max_rows", None)
+pd.set_option('display.max_columns', None)
+#pd.set_option('display.width', 100)
+print(df1)
+print(df2)
