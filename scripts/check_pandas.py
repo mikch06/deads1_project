11632cd1f736389365113ee2fe72524e99e0e817
mik <mail@kissel.ch>
2023-06-01 22:46:42 +0200

    work

--- scripts/check_pandas.py
+++ scripts/check_pandas.py
@@ -1,8 +1,8 @@
 import pandas as pd
-df = pd.read_json("../data/landing/data.ch.bfe.ladestellen-elektromobilitaet.json")
-print(df.shape)
-print(df.columns)
-df = pd.read_json("../data/landing/status.ch.bfe.ladestellen-elektromobilitaet.json")
-print(df.columns)
+# df = pd.read_json("../data/landing/data.ch.bfe.ladestellen-elektromobilitaet.json")
+# print(df.shape)
+# print(df.columns)
+# df = pd.read_json("../data/landing/status.ch.bfe.ladestellen-elektromobilitaet.json")
+# print(df.columns)
 df = pd.read_csv("../data/landing/ich_tanke_strom_monthly.csv")
 print(df.columns)
