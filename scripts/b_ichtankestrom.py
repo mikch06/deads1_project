b1583156aadd0362767da9204ea5b8109813fd2b
mikch06 <tools@kissel.ch>
2023-06-02 16:20:48 +0200

    add bronze statement for strom

--- scripts/b_ichtankestrom.py
+++ scripts/b_ichtankestrom.py
@@ -6,8 +6,8 @@ import pandas as pd
 df = pd.read_csv("../data/landing/ich_tanke_strom_monthly.csv")
 
 df1 = df.filter(regex="(year|month|locations_|stations_)")
-pd.set_option("display.max_rows", None)
-pd.set_option('display.max_columns', None)
+# pd.set_option("display.max_rows", None)
+# pd.set_option('display.max_columns', None)
 
 df2 = df.filter(regex="(year|month|stations_)")
 pd.set_option("display.max_rows", None)
@@ -15,3 +15,10 @@ pd.set_option('display.max_columns', None)
 pd.set_option('display.width', 100)
 print(df1)
 #print(df2)
+
+
+df1.to_csv("../data/bronze/b_ich_tanke_strom_monthly.csv", index=False)
+
+x, y = df['year'], df['month']
+
+print(y)
