dfd031a73d8285d180bd45725c3aa4beecc16003
mikch06 <tools@kissel.ch>
2023-06-02 08:56:45 +0200

    edit df

--- scripts/b_ichtankestrom.py
+++ scripts/b_ichtankestrom.py
@@ -5,13 +5,13 @@ import pandas as pd
 
 df = pd.read_csv("../data/landing/ich_tanke_strom_monthly.csv")
 
-df1 = df.filter(regex="(year|month|locations_)")
+df1 = df.filter(regex="(year|month|locations_|stations_)")
 pd.set_option("display.max_rows", None)
 pd.set_option('display.max_columns', None)
 
 df2 = df.filter(regex="(year|month|stations_)")
 pd.set_option("display.max_rows", None)
 pd.set_option('display.max_columns', None)
-#pd.set_option('display.width', 100)
+pd.set_option('display.width', 100)
 print(df1)
-print(df2)
+#print(df2)
