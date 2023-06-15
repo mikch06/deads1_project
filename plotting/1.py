2872c29d77976a65ccc09a3e04f0c05060b38b5b
mikch06 <tools@kissel.ch>
2023-06-15 17:02:45 +0200

    fixes for matplotlib example

--- plotting/1.py
+++ plotting/1.py
@@ -1,10 +1,9 @@
-from v_bokeh.plotting import figure, show, ColumnDataSource, output_file
 import pandas as pd
 import matplotlib.pyplot as plt
 
 # prepare some data
-x = [1, 2, 3, 4, 5]
-y = [6, 7, 2, 4, 5]
+# x = [1, 2, 3, 4, 5]
+# y = [6, 7, 2, 4, 5]
 # create a new plot with a title and axis labels
 # f = figure(title="Bokeh Simple line example", x_axis_label='x', y_axis_label='y')
 # # add a line renderer with legend and line thickness to the plot
@@ -12,36 +11,36 @@ y = [6, 7, 2, 4, 5]
 # show(f)
 
 # Simple line example (2) pandas
-#df = pd.read_csv("../data/bronze/b_ich_tanke_strom_monthly.csv")
+df = pd.read_csv("../data/bronze/b_ich_tanke_strom_monthly.csv")
 #lines = df.plot.line(title="Simple line example2")
 
 ##
 # Funktionierender multiline plot
 
-#plt.rcParams["figure.figsize"] = [7.50, 3.50]
-# plt.rcParams["figure.autolayout"] = True
-# df = pd.read_csv("../data/bronze/b_ich_tanke_strom_monthly.csv")
-# df.plot(title="Simple line example")
-# plt.show()
+plt.rcParams["figure.figsize"] = [7.50, 3.50]
+plt.rcParams["figure.autolayout"] = True
+df = pd.read_csv("../data/bronze/b_ich_tanke_strom_monthly.csv")
+df.plot(title="Simple line example")
+plt.show()
 
 
 
 
 ## Bokeh
 # Bokeh Versuch
-df = pd.read_csv("../data/bronze/b_ich_tanke_strom_monthly.csv")
+#df = pd.read_csv("../data/bronze/b_ich_tanke_strom_monthly.csv")
 #p = figure(title="Bokeh multiline example", x_axis_label='Zeit', y_axis_label='Anzahl')
 #source = ColumnDataSource(df)
-print(df.head())
-x = df['year']
-y = df['month']
+#print(df.head())
+#x = df['year']
+#y = df['month']
 
 #p.line(x, y, line_width=5)
 
 #output_file("html/foo.html")
-source = ColumnDataSource(df)
+#source = ColumnDataSource(df)
 #p = figure(x_axis_label='Zeit', y_axis_label='Anzahl', source=source)
-p = figure()
-p.line(x='stations_TG_count', y='stations_ZH_count', source=source)
-show(p)
+#p = figure()
+#p.line(x='stations_TG_count', y='stations_ZH_count', source=source)
+#show(p)
 
