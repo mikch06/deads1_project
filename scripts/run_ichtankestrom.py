3810c896ec640ea9588840f662b453412cd85d56
mikch06 <tools@kissel.ch>
2023-07-15 13:19:40 +0200

    init run_ichtankestrom.py

---
+++ scripts/run_ichtankestrom.py
@@ -0,0 +1,9 @@
+import s_ichtankestrom
+import v_ichtankestrom
+
+
+s_ichtankestrom.silver_ichtankestrom()
+print("silvering all data")
+
+v_ichtankestrom.visualize_ichtankestrom()
+print("plotting data")
