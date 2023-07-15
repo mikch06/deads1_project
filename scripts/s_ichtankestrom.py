d99cc2e33eb4607bb2a4734328c23fd6b4ab56ef
mikch06 <tools@kissel.ch>
2023-07-15 13:20:03 +0200

    function silver_ichtankestrom

--- scripts/s_ichtankestrom.py
+++ scripts/s_ichtankestrom.py
@@ -3,23 +3,24 @@ import pandas as pd
 # stations_XY_count : Anzahl Ladestationen pro Kanton.
 # locations_XY_count : Anzahl Standorte pro Kanton.
 
-df = pd.read_csv("../data/bronze/ich_tanke_strom_monthly.csv")
+def silver_ichtankestrom():
+    df = pd.read_csv("../data/bronze/ich_tanke_strom_monthly.csv")
 
-df1 = df.filter(regex="(year|month|locations_|stations_)")
-# pd.set_option("display.max_rows", None)
-# pd.set_option('display.max_columns', None)
+    df1 = df.filter(regex="(year|month|locations_|stations_)")
+    # pd.set_option("display.max_rows", None)
+    # pd.set_option('display.max_columns', None)
 
-print(df1)
-print("sample")
-print(df.sample(10))
+    print(df1)
+    print("sample")
+    print(df.sample(10))
 
-df2 = df.filter(regex="(year|month|stations_)")
-pd.set_option("display.max_rows", None)
-pd.set_option('display.max_columns', None)
-pd.set_option('display.width', 100)
-#print(df2)
+    df2 = df.filter(regex="(year|month|stations_)")
+    pd.set_option("display.max_rows", None)
+    pd.set_option('display.max_columns', None)
+    pd.set_option('display.width', 100)
+    #print(df2)
 
 
-df1.to_csv("../data/silver/b_ich_tanke_strom_monthly.csv", index=False)
+    df1.to_csv("../data/silver/b_ich_tanke_strom_monthly.csv", index=False)
 
 
