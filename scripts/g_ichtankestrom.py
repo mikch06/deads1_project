b1be438bf5f3aaea460730beaed48e2f1ef9cd97
mikch06 <tools@kissel.ch>
2023-07-16 17:07:01 +0200

    cut out CH sum, new files for ch stations and locations

--- scripts/g_ichtankestrom.py
+++ scripts/g_ichtankestrom.py
@@ -7,6 +7,15 @@ def gold_ichtankestrom():
     df = pd.read_csv("../data/silver/s_ich_tanke_strom_monthly.csv")
 
     df1 = df.filter(regex="(year|month|locations_|stations_)")
-
+    # Cut out locations and station all over CH
+    del df1["locations_CH_count"]
+    del df1["stations_CH_count"]
     df1.to_csv("../data/gold/g_ich_tanke_strom_monthly.csv", index=False)
 
+    # Sum CH locations
+    df2 = df.pop("locations_CH_count")
+    df2.to_csv("../data/gold/g_ich_tanke_strom_monthly_locations_CH.csv")
+
+    # Sum CH stations
+    df3 = df.pop("stations_CH_count")
+    df3.to_csv("../data/gold/g_ich_tanke_strom_monthly_stations_CH.csv")
