9c0dfaa9c33654a73715d821132c7cb8ff81c441
mikch06 <tools@kissel.ch>
2023-08-13 16:25:57 +0200

    Beautify code

--- scripts/g_energy.py
+++ scripts/g_energy.py
@@ -4,12 +4,14 @@ import pandas as pd
 # locations_XY_count : Anzahl Standorte pro Kanton.
 
 def gold_energy():
+    # Stromprudktion
     df = pd.read_csv("../data/silver/s_stromprduktion.csv")
-
-
     df.to_csv("../data/gold/g_stromproduktion.csv", index=False)
 
-
+    # Landesverbrauch estimated!
     df1 = pd.read_csv("../data/silver/s_landesverbrauch-estimated.csv")
-
     df1.to_csv("../data/gold/g_landesverbrauch-estimated.csv", index=False)
+
+    # Landesverbrauch / Endverbrauch
+    df2 = pd.read_csv("../data/silver/s_landesverbrauch-endverbrauch.csv")
+    df2.to_csv("../data/gold/g_landesverbrauch-endverbrauch.csv", index=False)
