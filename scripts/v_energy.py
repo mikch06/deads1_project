899edda55ca01fe42cfc402f5fd161e02a976672
mikch06 <tools@kissel.ch>
2023-08-13 14:37:49 +0200

    Beautify code

--- scripts/v_energy.py
+++ scripts/v_energy.py
@@ -6,20 +6,11 @@ from bokeh.palettes import Viridis256
 import pandas as pd
 
 def visualize_energy():
+    # Stromproduktion
     df = pd.read_csv("../data/gold/g_stromproduktion.csv")
-    # html = df.to_html(border=1)
-    # print("HTML Output:", html)
-    #
-    # output_html = open("/var/www/deads/html/plots/v_stromproduktion.html", "w")
-    # output_html.write(html)
-    # output_html.close()
     df.to_html('/var/www/deads/html/plots/v_stromproduktion.html', index=False, border=0, justify='left', classes='')
 
-
-
-
-
-
+    # Landesverbrauch (estimated)
     df1 = pd.read_csv("../data/gold/g_landesverbrauch-estimated.csv")
     html = df1.to_html(border=1)
     print("HTML Output:", html)
