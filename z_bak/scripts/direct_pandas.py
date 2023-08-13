7b18919ce0ddd652e4b1da8a9113ef5312528fc7
mikch06 <tools@kissel.ch>
2023-08-13 14:38:07 +0200

    remove old backup data

--- z_bak/scripts/direct_pandas.py
+++
@@ -1,3 +0,0 @@
-import pandas as pd
-df = pd.read_json("https://data.geo.admin.ch/ch.bfe.ladestellen-elektromobilitaet/data/oicp/ch.bfe.ladestellen-elektromobilitaet.json")
-print(df.tail)
--- z_bak/scripts/snippets.py
+++
@@ -1,7 +0,0 @@
-# ...
-x, y = df['year'], df['month']
-
-# pandas plot / matplotlib.pyplot
-df1.plot()
-import matplotlib.pyplot as plt
-plt.show()
