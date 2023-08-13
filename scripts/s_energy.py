ecc84d6e51929033f1cfd3b8240cfbefa0bd7674
mikch06 <tools@kissel.ch>
2023-08-13 16:25:46 +0200

    Beautify code

--- scripts/s_energy.py
+++ scripts/s_energy.py
@@ -4,22 +4,17 @@ import pandas as pd
 
 def silver_energy():
     df = pd.read_csv("../data/bronze/b_stromproduktion.csv")
-
     #df1 = df
     # pd.set_option("display.max_rows", None)
     # pd.set_option('display.max_columns', None)
-
     print(df)
     # print("sample")
     # print(df.sample(10))
-
     df.to_csv("../data/silver/s_stromprduktion.csv", index=False)
-
 
     df1 = pd.read_csv("../data/bronze/b_landesverbrauch-endverbrauch.csv")
     print(df1)
     df1.to_csv("../data/silver/s_landesverbrauch-endverbrauch.csv")
-
 
     df2 = pd.read_csv("../data/bronze/b_landesverbrauch-estimated.csv")
     print(df2)
